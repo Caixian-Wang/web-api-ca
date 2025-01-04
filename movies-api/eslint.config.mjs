af48927174984548d2c1037d3783e1984e9a1ccd
Caixian-Wang <20108795@mail.wit.ie>
2025-01-04 16:46:37 +0000

    Created CA2 repository; added README template

--- movies-api/eslint.config.mjs
+++ movies-api/eslint.config.mjs
@@ -2,7 +2,7 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 
 /** @type {import('eslint').Linter.Config[]} */
-export default [
+module.exports = [
   {
     files: ["**/*.js"],
     languageOptions: {
