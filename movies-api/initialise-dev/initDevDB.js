1c8546fb908e17b373c23328c04307ab1b2ce806
Caixian-Wang <20108795@mail.wit.ie>
2025-01-05 05:34:59 +0000

    Add reviews set API

--- movies-api/initialise-dev/initDevDB.js
+++ movies-api/initialise-dev/initDevDB.js
@@ -3,8 +3,10 @@ dotenv.config();
 import mongoose from 'mongoose';
 import users from './users';
 import movies from './movies';
+import reviews from './reviews'; // 导入 reviews 数据
 import User from '../api/users/userModel';
 import Movie from '../api/movies/movieModel';
+import Review from '../api/reviews/reviewModel'; // 导入 Review 模型
 
 async function main() {
     if (process.env.NODE_ENV !== 'development') {
@@ -15,11 +17,17 @@ async function main() {
     // Drop collections
     await User.collection.drop().catch(err => console.log('User collection not found'));
     await Movie.collection.drop().catch(err => console.log('Movie collection not found'));
+    await Review.collection.drop().catch(err => console.log('Review collection not found')); // 删除 reviews 集合
+
     await User.create(users);
     await Movie.create(movies);
+    await Review.create(reviews); // 添加 reviews 数据
+
     console.log('Database initialised');
     console.log(`${users.length} users loaded`);
     console.log(`${movies.length} movies loaded`);
+    console.log(`${reviews.length} reviews loaded`); // 输出 reviews 加载信息
+
     await mongoose.disconnect();
 }
 
---
+++ movies-api/initialise-dev/reviews.js
@@ -0,0 +1,39 @@
+[
+    {
+      "movieId": 671583,
+      "author": "user1",
+      "content": "Amazing movie with a gripping storyline! Highly recommend.",
+      "rating": 9
+    },
+    {
+      "movieId": 590706,
+      "author": "user1",
+      "content": "The cinematography was stunning, but the plot felt a bit slow.",
+      "rating": 7
+    },
+    {
+      "movieId": 671583,
+      "author": "Charlie",
+      "content": "Not my cup of tea, but the performances were decent.",
+      "rating": 5
+    },
+    {
+      "movieId": 590706,
+      "author": "Alice",
+      "content": "A masterpiece! The director did a fantastic job.",
+      "rating": 10
+    },
+    {
+      "movieId": 671583,
+      "author": "user1",
+      "content": "Good movie, but a bit predictable in some parts.",
+      "rating": 8
+    },
+    {
+      "movieId": 590706,
+      "author": "user1",
+      "content": "A fresh take on a classic genre. I enjoyed it a lot.",
+      "rating": 8
+    }
+  ]
+  
