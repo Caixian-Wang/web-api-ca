e0f9887306f019bf47b271cf819e866ac3b5d629
Caixian-Wang <20108795@mail.wit.ie>
2025-01-05 05:53:58 +0000

    Complete the comment function

--- movies-api/index.js
+++ movies-api/index.js
@@ -6,7 +6,7 @@ import './db';
 import defaultErrHandler from './errHandler'
 import moviesRouter from './api/movies';   //import movies router
 import authenticate from './authenticate';
-
+import reviewsRouter from './api/reviews'
 dotenv.config();
 
 const app = express();
@@ -18,7 +18,7 @@ app.use('/api/users', usersRouter);
 app.use(defaultErrHandler);
 app.use('/api/movies', moviesRouter); //ADD THIS BEFORE THE DEFAULT ERROR HANDLER.
 app.use('/api/movies', authenticate, moviesRouter);
-
+app.use('/api/reviews', reviewsRouter);
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
 });
